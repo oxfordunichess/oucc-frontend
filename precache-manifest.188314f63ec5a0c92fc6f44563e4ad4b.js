41678933f7f07dec81933809cb3ba59e954d8db1
theLAZYmd <lazymeep+github@gmail.com>
2019-10-21 01:40:50 +0100

    Updates

---
+++ precache-manifest.188314f63ec5a0c92fc6f44563e4ad4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02f7ef1f8abde8a6373b494fec8e6efe",
+    "url": "/oucc-frontend/index.html"
+  },
+  {
+    "revision": "8887cf31e99ed70918b1",
+    "url": "/oucc-frontend/static/css/main.754bf405.chunk.css"
+  },
+  {
+    "revision": "12760a5b88ae25cdcb11",
+    "url": "/oucc-frontend/static/js/2.ec8934ed.chunk.js"
+  },
+  {
+    "revision": "8887cf31e99ed70918b1",
+    "url": "/oucc-frontend/static/js/main.ea44d447.chunk.js"
+  },
+  {
+    "revision": "8833e4877213f454dead",
+    "url": "/oucc-frontend/static/js/runtime~main.a3a1317e.js"
+  }
+]);
