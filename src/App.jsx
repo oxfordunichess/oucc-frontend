8f67ee42b851d6ee1ed3c99d9a0d287ebd8f73d9
theLAZYmd <lazymeep+github@gmail.com>
2019-10-24 14:53:37 +0100

    Added sponsors, news, tournament

--- src/App.jsx
+++ src/App.jsx
@@ -28,9 +28,10 @@ export default class App extends React.Component {
 			<Router basename={process.env.PUBLIC_URL}>
 				<Switch>
 					<Route exact path='/' render={() => <Page page='main' />} />
-					<Route exact path='/members' render={() => <Redirect to='/membership' />} />
 					<Route exact path='/contact' render={() => <Contact />}/>
+					<Route exact path='/members' render={() => <Redirect to='/membership' />} />
 					<Route exact path='/index.html' render={() => <Redirect to='/' />} />
+					<Route exact path='/news' render={() => <Redirect to='/curr_news' />} />
 					<Route path='/termcard' render={() => {
 						window.open('https://unioxfordnexus-my.sharepoint.com/:x:/g/personal/chri5551_ox_ac_uk/EQAUw-Mpt6pMoKx93Y8MFM0BQSwz7Xsqx1vJ54r5SILOrA')
 						return <Redirect to='/leagues'/>
@@ -43,11 +44,14 @@ export default class App extends React.Component {
 						'archive2',
 						'classes',
 						'curr_news',
+						'old_news',
 						'leagues',
 						'maillists',
 						'main',
 						'membership',
-						'termcard',
+						'sponsors',
+						//'termcard',
+						'tournament',
 						'varsity'
 					].map(path => <Route exact key={path + '_route'} path={'/' + path} render={(props) => <Page {...props} page={path} />} />)}
 					<Route exact path='/committee' render={(props) => <Table {...props} file={'committee2019'} />} />
