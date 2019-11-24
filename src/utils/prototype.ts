b89bcdd77cbe90d553f9951a67f779105208320c
theLAZYmd <lazymeep+github@gmail.com>
2019-11-24 22:06:08 +0000

    .

--- src/utils/prototype.ts
+++ src/utils/prototype.ts
@@ -1,4 +1,14 @@
+import regexes from './regexes';
+
 export function capitalise(str: string): string {
 	if (typeof str !== 'string') throw new TypeError();
 	return str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+export function getIDFromTitle(str :string): string {
+	while (str.startsWith('#')) {
+		str = str.slice(1);
+	}
+	str = str.trim();
+	return str.match(regexes.letters).join('-').toLowerCase();
 }
