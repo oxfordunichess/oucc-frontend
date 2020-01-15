d4d79791c2466a60b0df108c7193a54cfff1c760
theLAZYmd <lazymeep+github@gmail.com>
2020-01-15 13:05:11 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/oucc-frontend/precache-manifest.245f954529dc3345e9a6849fc6802608.js"
+  "/oucc-frontend/precache-manifest.35c7d7db562c3de4d35601746c72d093.js"
 );
 
 self.addEventListener('message', (event) => {
