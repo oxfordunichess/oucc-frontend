3cfa2a5092cb929607d0a4010f5a783f7dc3c8e6
theLAZYmd <theLAZYmd@gmail.com>
2021-04-11 06:54:09 +0100

    Added link in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,15 +9,22 @@ export default class Footer extends React.Component {
 	render(): ReactElement {
 		return (
 			<div className={styles.footer}>
-				<Link className={styles.link} href='https://www.facebook.com/oxfordunichess/'>
-					{facebook}
-				</Link>
-				<Link className={styles.link} href='https://www.facebook.com/groups/oxford.chess.club/'>
-					{facebook}
-				</Link>
-				<Link className={styles.link} href='https://mailchi.mp/e4bab2111329/oxfordunichess'>
-					{mail}
-				</Link>
+				<div className={styles.icons}>
+					<Link className={styles.link} href='https://www.facebook.com/oxfordunichess/'>
+						{facebook}
+					</Link>
+					<Link className={styles.link} href='https://www.facebook.com/groups/oxford.chess.club/'>
+						{facebook}
+					</Link>
+					<Link className={styles.link} href='https://mailchi.mp/e4bab2111329/oxfordunichess'>
+						{mail}
+					</Link>
+				</div>
+				<div className={styles.tag}>
+					<Link href='https://github.com/oxfordunichess/oucc-backend/tree/master/pages'>
+						Edit these pages
+					</Link>
+				</div>		
 			</div>
 		);
 	}
